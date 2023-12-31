79b38d31d773222296265680cab241a329c8cbab
StoreaGroup <hi@storea.ir>
2024-01-01 00:40:00 +0330

    Code Updated and workd;

--- py.py
+++ py.py
@@ -1,25 +1,61 @@
+# service.1f98235496c2471da6a8997d0b474c2c
 import requests
+API_KEY = "service.1f98235496c2471da6a8997d0b474c2c" 
 
-API_KEY = "service.1f98235496c2471da6a8997d0b474c2c"
-
-search_url = "https://api.neshan.org/v1/search"
+api_url = "https://api.neshan.org/v1/search"
 
 params = {
-  "term": "کافی شاپ",
-  "lat": 34.6401, 
-  "lng": 50.8782,
-  "limit": 50, 
-  "api_key": API_KEY  
+  "term": "coffee",
+  "lat": 35.6998,
+  "lng": 51.3380, 
+  "apikey": API_KEY
 }
 
-results = requests.get(search_url, params=params).json()
+headers = {
+  "Api-Key": API_KEY
+}
 
-cafes = results.get("items") or []
+response = requests.get(api_url, params=params, headers=headers)
+results = response.json()
 
-for cafe in cafes:
-  name = cafe.get("title")  
-  address = cafe.get("address")
+print(f"Number of results found: {results['count']}")
+
+for item in results["items"]:
+  print(item["title"])
+  print(item["address"])
+  print("-"*30)
+
+  html_content = """
+<html>
+<head>
+<title>جستجو</title>  
+</head>
+
+<body>
+
+<h1>نتایج جستجو</h1>
+
+<ul>
+"""
+
+for item in results["items"]:
+  title = item["title"]
+  address = item["address"]
   
-  print(name, address)
+  html_content += f"<li>{title} <br> {address}</li>"
 
+html_content += """
+</ul>
 
+</body>
+</html>
+"""
+
+# ذخیره متن HTML در فایل
+
+# with open("results.html","w") as f:
+#   f.write(html_content)
+
+with open("results.html","w", encoding="utf-8") as f:
+  f.write(html_content) 
+print("Results saved in results.html")
